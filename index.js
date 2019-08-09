6290c28ba9da2279675492b8d318b8e4a83a405a
Richard Ryan Garcia <richard.garcia.b@heb.com>
2019-08-09 18:53:13 -0500

    modified index to handle parameters and updated find to handle custom bytes

--- index.js
+++ index.js
@@ -1,58 +1,47 @@
 keccak256 = require('js-sha3').keccak256;
+const { readdirSync } = require('fs');
+
 module.exports = async (config) => {
-  var CHARS = '0123456789abcdefghijklmnopqrstuvwxysABCDEFGHIJKLMNOPQRSTUVWXYS$_'.split('');
-  var CHAR_MAP = {};
-  CHARS.forEach(function (c, index) {
-    CHAR_MAP[c] = index;
-  });
-
-  var CHAR_CODES = CHARS.map(function (c) {
-    return c.charCodeAt(0);
-  });
-
-  var CHAR_CODE_MAP = {};
-  CHARS.forEach(function (c, index) {
+  const args = config._;
+  const contract = config.c;
+  const numberOfBytes = config.b || 2 ;
+  const functions = args.slice(1,args.length);
+  const contractsBuildDir = config.contracts_build_directory;
+  let data = { blocks: [], s: [] };
+  let CHARS = '0123456789abcdefghijklmnopqrstuvwxysABCDEFGHIJKLMNOPQRSTUVWXYS$_'.split('');
+  let CHAR_CODE_MAP = {};
+  CHARS.forEach((c, index) => {
     CHAR_CODE_MAP[index] = c.charCodeAt(0);
   });
-  var data = { blocks: [], s: [] };
-  function save(hash) {
-    data.reset = hash.reset;
-    data.block = hash.block;
-    data.start = hash.start;
-    data.finalized = hash.finalized;
-    data.lastByteIndex = hash.lastByteIndex;
-    for (var i = 0; i < hash.blocks.length; ++i) {
-        data.blocks[i] = hash.blocks[i];
-    }
-    for (var i = 0; i < hash.s.length; ++i) {
-        data.s[i] = hash.s[i];
-    }
+  let functionSignatures = [];
+
+  const gatherSignatures = (buildDir, contractName) => {
+    const artifactPath = `${buildDir}/${contractName}`
+    const artifact = require(artifactPath)
+    artifact.abi.forEach(contractArtifact => {
+      if (contractArtifact.type === 'function' && contractArtifact.stateMutability != 'view'){
+        let types = contractArtifact.inputs.map(input => input.type);
+        functionSignatures.push(`${contractArtifact.name}(${types})`)
+      }
+    })
   }
-  function restore(hash) {
-    hash.reset = data.reset;
-    hash.block = data.block;
-    hash.start = data.start;
-    hash.finalized = data.finalized;
-    hash.lastByteIndex = data.lastByteIndex;
-    for (var i = 0; i < data.blocks.length; ++i) {
-        hash.blocks[i] = data.blocks[i];
-    }
-    for (var i = 0; i < data.s.length; ++i) {
-        hash.s[i] = data.s[i];
-    }
+
+  const selectSignatures = (buildDir, contractName, functionNames) => {
+    const artifactPath = `${buildDir}/${contractName}`
+    const artifact = require(artifactPath)
+    artifact.abi.forEach(contractArtifact => {
+      if (contractArtifact.type === 'function' && contractArtifact.stateMutability != 'view' && functionNames.includes(contractArtifact.name)){
+        let types = contractArtifact.inputs.map(input => input.type);
+        functionSignatures.push(`${contractArtifact.name}(${types})`)
+      }
+    })
   }
-  function toBytes(str) {
-    var bytes = [];
-    for (var i = 0; i < str.length; ++i) {
-        bytes.push(str.charCodeAt(i));
-    }
-    return bytes;
-  }
-  function parseSignature(signature) {
+
+  const parseSignature = signature => {
     if (signature.charAt(signature.length - 1) != ')' || signature.indexOf(' ') !== -1) {
         return false;
     }
-    var parts = signature.split('(');
+    let parts = signature.split('(');
     if (parts.length == 2) {
         return {
         name: parts[0],
@@ -62,47 +51,21 @@ module.exports = async (config) => {
         return false;
     }
   }
-  function increase(bytes) {
-    bytes[0] += 1;
-    for (var i = 0; i < bytes.length; ++i) {
-        if (bytes[i] === 64) {
-        bytes[i] = 0;
-        if (i == bytes.length - 1) {
-            bytes[i + 1] = 1;
-        } else {
-            bytes[i + 1] += 1;
-        }
-        } else {
-        break;
-        }
-    }
-    return bytes;
-  }
-  function toChars(bytes) {
-    var str = '';
-    for (var i = 0; i < bytes.length; ++i) {
-        str += CHARS[bytes[i]];
-    }
-    return str;
-  }
-  function toCharCodes(bytes) {
-    var codes = [];
-    for (var i = 0; i < bytes.length; ++i) {
-        codes.push(CHAR_CODE_MAP[bytes[i]]);
-    }
-    return codes;
-  }
-  function find(obj) {
-    var sig = obj.name + obj.args;
-    var args = toBytes(obj.args);
-    var bytes = [0];
-    var index = 0;
-    var prefix = toBytes(obj.name + '_');
-    var hash = keccak256.create();
+
+
+  const find = obj => {
+    let orig_sig = obj.name + obj.args
+    let sig = obj.name + obj.args;
+    let args = toBytes(obj.args);
+    let bytes = [0];
+    let index = 0;
+    let prefix = toBytes(obj.name + '_');
+    let hash = keccak256.create();
     hash.update(prefix);
     save(hash);
-    var char, methodId = keccak256.array(sig);
-    while (methodId[0] || methodId[1]) {
+    let char, methodId = keccak256.array(sig);
+    
+    while (methodId[0]|| methodId[1]) {
         if (index >= CHARS.length) {
         increase(bytes);
         hash = keccak256.create();
@@ -121,41 +84,102 @@ module.exports = async (config) => {
     if (index) {
         sig = obj.name + '_' + toChars(bytes) + char + obj.args;
     }
-    return [sig, keccak256(sig).substr(0, 8)];
+    return [orig_sig, sig, keccak256(sig).substr(0, 8)];
   }
+
+  const toBytes = str => {
+    let bytes = [];
+    for (let i = 0; i < str.length; ++i) {
+        bytes.push(str.charCodeAt(i));
+    }
+    return bytes;
+  }
+
+  const toChars = bytes => {
+    let str = '';
+    for (let i = 0; i < bytes.length; ++i) {
+        str += CHARS[bytes[i]];
+    }
+    return str;
+  }
+
+  const toCharCodes = bytes => {
+    let codes = [];
+    for (let i = 0; i < bytes.length; ++i) {
+        codes.push(CHAR_CODE_MAP[bytes[i]]);
+    }
+    return codes;
+  }
+
+  const save = hash => {
+    data.reset = hash.reset;
+    data.block = hash.block;
+    data.start = hash.start;
+    data.finalized = hash.finalized;
+    data.lastByteIndex = hash.lastByteIndex;
+    for (let i = 0; i < hash.blocks.length; ++i) {
+        data.blocks[i] = hash.blocks[i];
+    }
+    for (let i = 0; i < hash.s.length; ++i) {
+        data.s[i] = hash.s[i];
+    }
+  }
+
+  const restore = hash => {
+    hash.reset = data.reset;
+    hash.block = data.block;
+    hash.start = data.start;
+    hash.finalized = data.finalized;
+    hash.lastByteIndex = data.lastByteIndex;
+    for (let i = 0; i < data.blocks.length; ++i) {
+        hash.blocks[i] = data.blocks[i];
+    }
+    for (let i = 0; i < data.s.length; ++i) {
+        hash.s[i] = data.s[i];
+    }
+  }
+
+  const increase = bytes => {
+    bytes[0] += 1;
+    for (let i = 0; i < bytes.length; ++i) {
+        if (bytes[i] === 64) {
+        bytes[i] = 0;
+        if (i == bytes.length - 1) {
+            bytes[i + 1] = 1;
+        } else {
+            bytes[i + 1] += 1;
+        }
+        } else {
+        break;
+        }
+    }
+    return bytes;
+  }
+
 /////
- let ouputs;
- const contractName = config._[1]
- const priorities = config._.slice(2,config._.length)
- const contractsBuildDir = config.contracts_build_directory
- const artifactPath = `${contractsBuildDir}/${contractName}.json`
- const artifact = require(artifactPath)
- var sources = artifact.abi.reduce(function(result, func) {
-  if (priorities.includes(func.name)) {
-    let types = func.inputs.map(input => input.type)
-    result.push(`${func.name}(${types})`);
+
+if (contract) {
+  if (functions && functions.length > 0 ){
+    selectSignatures(contractsBuildDir, contract, functions)
+  } else {
+    gatherSignatures(contractsBuildDir, contract)
   }
-  return result;
-}, []);
-let result;
-let suggestions = [];
-if (sources && sources.length > 0){
-  let r;
-  for (r=0; r < sources.length; r++){
-    let a = parseSignature(sources[r]);
+
+  console.log(`will run ${functionSignatures.length} functions from ${contract} contract`)
+} else { 
+  const files = readdirSync(contractsBuildDir);
+  files.forEach(contractName => {
+    gatherSignatures(contractsBuildDir, contractName)
+  })
+  console.log(`will run ${functionSignatures.length} functions across ${files.length} contracts`)
+}
+
+  let suggestions = [];
+  for (let i=0; i < functionSignatures.length; i++){
+    let a = parseSignature(functionSignatures[i]);
     result = find(a);
     suggestions.push(result)
   }
-}
-  let i;
-  let outputs = [];
-  for(i = 0; i < sources.length;i++ ){
-    outputs.push({
-      oldName: sources[i], 
-      newName: suggestions[i][0]
-    })
-  }
- outputs.forEach(output => {
-   console.log(output.oldName, output.newName)
- })
+
+  console.log(suggestions)
 }
